49c58d756e9470a4f02edff086032e016e5ec231
cranada333 <cranada333@gmail.com>
2026-03-04 18:21:58 +0900

    Add files via upload

---
+++ LAB 2.1/LAB 2.1/소스.cpp
@@ -0,0 +1,10 @@
+// myfirst.cpp -- displays a message
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Come up and C++ me some time."; //;를 활용해 문장을 마무리
+	cout << endl; //줄을 변환
+	cout << "You won't regret it!";
+	return 0;
+	}
